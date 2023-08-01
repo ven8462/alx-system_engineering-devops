04f8c87cbd7d711af6a671d04e6cd4d15a9b0fb2
lavender <lavenderanyango9@gmail.com>
2023-08-02 00:27:48 +0300

    trial

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.+?)\]\s\[to:(.+?)\]\s\[flags:((-?\d+:?)+)/).join(',')
+puts ARGV[0].scan(/\[from:(.\w*)\]\s\[to:(\W*\d*)\]\s\[flags:((?:-?\d:?)+)/).join(separator=',')
