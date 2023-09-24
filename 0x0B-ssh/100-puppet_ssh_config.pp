99d1abbd49cb287139459531a3372a675c278804
lavender <lavenderanyango9@gmail.com>
2023-09-24 23:55:03 +0300

    fix puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,9 @@
-# puppet to log into a server without a password.
-
-class { 'ssh::client':
-  options => {
-    'Host *' => {
-      'PasswordAuthentication' => 'no',
-      'IdentityFile' => '/home/codebind/.ssh/school',
-    },
-  },
+# Puppet to set changes in a server
+file_line { 'disable password login':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
+file_line { 'add path to find the keys':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/holberton',
 }
