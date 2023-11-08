8ff4e451d9ddc77ecd25cedcd81df84fd4e6da96
lavender <lavenderanyango9@gmail.com>
2023-11-08 11:44:01 +0300

    . Recurse it

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+ Reddit APIs
+"""
+from requests import get
+
+
+def top_ten(subreddit):
+    """queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit.
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {
+        'User-Agent':'My User Agent 2'
+    }
+    response = get(url, headers=headers, allow_redirects=False)
+    reddits = response.json()
+
+    try:
+        children = reddits.get('data').get('children')
+        for i in range(10):
+            print(children[i].get('data').get('title'))
+    except:
+        print('None')
